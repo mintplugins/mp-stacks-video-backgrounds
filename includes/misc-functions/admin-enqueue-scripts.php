0ec48840ee116a4f472c41bb0d187195a82a0587
Phil Johnston <support@mintplugins.com>
2015-09-17 15:42:07 -0400

    Admin Meta Scripts now enqueued only when needed.

---
+++ includes/misc-functions/admin-enqueue-scripts.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * MP Stacks + Video Backgrounds Enqueue Scripts
+ *
+ * @link http://mintplugins.com/doc/
+ * @since 1.0.0
+ *
+ * @package    MP Stacks + Video Backgrounds
+ * @subpackage Functions
+ *
+ * @copyright   Copyright (c) 2015, Mint Plugins
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @author      Philip Johnston
+ */
