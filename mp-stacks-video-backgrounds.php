0d64626048c6464ff026b06ad7af7ed8764fc4b1
Phil Johnston <support@mintplugins.com>
2016-11-14 12:27:49 -0500

    1.0.0.4 Version Bump

--- mp-stacks-video-backgrounds.php
+++ mp-stacks-video-backgrounds.php
@@ -3,7 +3,7 @@
 Plugin Name: MP Stacks + Video Backgrounds
 Plugin URI: http://mintplugins.com/plugins/mp-stacks-video-backgrounds
 Description: Add aBackground Video to any Brick in MP Stacks.
-Version: 1.0.0.3
+Version: 1.0.0.4
 Author: Mint Plugins
 Author URI: http://mintplugins.com
 Text Domain: mp_stacks_video_backgrounds
@@ -13,18 +13,18 @@ License: GPL2
 
 /*  Copyright 2015  Phil Johnston  (email : phil@mintplugins.com)
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Mint Plugins Core.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Mint Plugins Core.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Mint Plugins Core, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Mint Plugins Core, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 /*
@@ -34,7 +34,7 @@ License: GPL2
 */
 // Plugin version
 if( !defined( 'MP_STACKS_VIDEO_BACKGROUNDS_VERSION' ) )
-	define( 'MP_STACKS_VIDEO_BACKGROUNDS_VERSION', '1.0.0.3' );
+	define( 'MP_STACKS_VIDEO_BACKGROUNDS_VERSION', '1.0.0.4' );
 
 // Plugin Folder URL
 if( !defined( 'MP_STACKS_VIDEO_BACKGROUNDS_PLUGIN_URL' ) )
@@ -102,28 +102,28 @@ function mp_stacks_video_backgrounds_include_files(){
 	 * If mp_core isn't active, stop and install it now
 	 */
 	if (!function_exists('mp_core_textdomain')){
-		
+
 		/**
 		 * Include Plugin Checker
 		 */
 		require( MP_STACKS_VIDEO_BACKGROUNDS_PLUGIN_DIR . '/includes/plugin-checker/class-plugin-checker.php' );
-		
+
 		/**
 		 * Include Plugin Installer
 		 */
 		require( MP_STACKS_VIDEO_BACKGROUNDS_PLUGIN_DIR . '/includes/plugin-checker/class-plugin-installer.php' );
-		
+
 		/**
 		 * Check if wp_core in installed
 		 */
 		include_once( MP_STACKS_VIDEO_BACKGROUNDS_PLUGIN_DIR . 'includes/plugin-checker/included-plugins/mp-core-check.php' );
-		
+
 	}
 	/**
 	 * If mp_core is active but mp_stacks isn't, stop and install it now
 	 */
 	elseif(!function_exists('mp_stacks_textdomain')){
-		
+
 		/**
 		 * Check if wp_html_in_post in installed
 		 */
@@ -133,27 +133,27 @@ function mp_stacks_video_backgrounds_include_files(){
 	 * Otherwise, if mp_core and mp_stacks are installed, carry out the plugin's functions
 	 */
 	else{
-			
+
 		/**
 		 * Update script - keeps this plugin up to date
 		 */
 		require( MP_STACKS_VIDEO_BACKGROUNDS_PLUGIN_DIR . 'includes/updater/mp-stacks-video-backgrounds-update.php' );
-		
+
 		/**
 		 * Enqueue Scripts
 		 */
 		require( MP_STACKS_VIDEO_BACKGROUNDS_PLUGIN_DIR . 'includes/misc-functions/admin-enqueue-scripts.php' );
-		
+
 		/**
 		 * Hook callback for video background
 		 */
 		require( MP_STACKS_VIDEO_BACKGROUNDS_PLUGIN_DIR . 'includes/misc-functions/mp-stacks-hooks.php' );
-		
+
 		/**
 		 *  Video Backgrounds Metabox
 		 */
 		require( MP_STACKS_VIDEO_BACKGROUNDS_PLUGIN_DIR . 'includes/metaboxes/video-backgrounds/video-backgrounds.php' );
-		
+
 		/**
 		 * Add this add on to the list of Active MP Stacks Add Ons
 		 */
@@ -164,7 +164,7 @@ function mp_stacks_video_backgrounds_include_files(){
 			}
 			add_filter( 'mp_stacks_active_add_ons', 'mp_stacks_video_backgrounds_add_active' );
 		}
-		
+
 	}
 }
 add_action('plugins_loaded', 'mp_stacks_video_backgrounds_include_files', 9);
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: johnstonphilip
 Donate link: http://mintplugins.com/
 Tags: page, builder, stacks, bricks
 Requires at least: 3.5
-Tested up to: 4.1
-Stable tag: 1.0.0.3
+Tested up to: 4.6
+Stable tag: 1.0.0.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -20,7 +20,7 @@ This section describes how to install the plugin and get it working.
 
 1. Upload the 'mp-stacks-video-backgrounds’ folder to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Build Bricks under the “Stacks and Bricks” menu. 
+3. Build Bricks under the “Stacks and Bricks” menu.
 4. Publish your bricks into a “Stack”.
 5. Put Stacks on pages using the shortcode or the “Add Stack” button.
 
@@ -33,6 +33,10 @@ See full instructions at http://mintplugins.com/doc/mp-stacks
 
 == Changelog ==
 
+= 1.0.0.4 = November 14, 2016
+* Mute mp4s by default. Also added playsinline attr
+* Add esc_attr
+
 = 1.0.0.3 = September 17, 2015
 * Removed localhost as the origin in youtube.js
 * Added color overlay over background videos so you can add tint to the background video.
@@ -40,13 +44,13 @@ See full instructions at http://mintplugins.com/doc/mp-stacks
 
 = 1.0.0.2 = May 15, 2015
 * Proper video sizing for smaller devices.
-* Make videos truly in the background so if you double click the brick, it doesn't affect the video. 
+* Make videos truly in the background so if you double click the brick, it doesn't affect the video.
 * Added new Meta Field for custom video URL so users can easily upload their own video using the WP Media Library.
 
 = 1.0.0.1 = November 20, 2014
 * Put the video BG meta options in a showhider to organize the options better.
 * Made it so videos don’t show if google bot - because it slows down the index.
-* Added HTML Changelog 
+* Added HTML Changelog
 
 = 1.0.0.0 = August 7, 2014
 * Original release
